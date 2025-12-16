712c2d2c548d9a0545170fc5c2db240b4d4c2f98
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-16 15:06:35 +0100

    tanti bottoni risposte quanto risposte

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,3 +1,124 @@
-function calcTimer(divId) {
-    // document.getElementById(divId).textContent = nuovoTempo
+const questions = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does CPU stand for?",
+    correct_answer: "Central Processing Unit",
+    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+    countdownSecondi: 60,
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+    countdownSecondi: 40,
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+    countdownSecondi: 60,
+  },
+];
+
+let indiceDomandaAttuale = 0;
+
+window.addEventListener("load", () => {
+  addEventListeners();
+});
+
+function addEventListeners() {
+  const bottoniRisposta = document.querySelectorAll(".risposte > .bottoni");
+  bottoniRisposta.forEach((bottone) => {
+    bottone.addEventListener("click", handleClickBottoneRisposta);
+  });
+}
+
+function handleClickBottoneRisposta(ev) {
+  passaAProssimaDomanda();
+}
+
+const passaAProssimaDomanda = function () {
+  if (haiTerminatoDomande()) {
+    // passa alla prossima pagina
+    // window.location = "./results.html"
+    return 
+  }
+
+  const indiceProssimaDomanda = indiceDomandaAttuale + 1;
+  const prossimaDomanda = questions[indiceProssimaDomanda];
+  //   incremento il contatore attuale
+  indiceDomandaAttuale += 1;
+
+  aggiornaDomandaUI(prossimaDomanda)
+
+//    attivaCountdownUIIn();
+
+  // verifica che le domande non siano già arrivate alla fine
+  // if () {
+
+  // }
+};
+
+// SCHEMA DOMANDA
+//   {
+//     category: "Science: Computers",
+//     type: "multiple",
+//     difficulty: "easy",
+//     question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+//     correct_answer: "Final",
+//     incorrect_answers: ["Static", "Private", "Public"],
+//     countdownSecondi: 40,
+//   },
+function aggiornaDomandaUI(domandaObj) {
+    // prendi l'html di interesse
+    // html domanda
+    const domandaEl = document.querySelector(".question > h1")
+    domandaEl.textContent = domandaObj.question
+    const tutteRisposte = ottieniTutteRisposte(domandaObj)
+    let indiceRisposta = 0
+    
+    const bottoniRisposteEl = document.querySelectorAll(".risposte > .bottoni")
+
+    // siccome alcune domande hanno un numero variabili di risposte, 
+    // nello specifico o 2 o 4, allora vanno eliminati gli elementi html
+    // in più. si assume che l'html delle risposte avrà sempre 4 elementi html.
+    const secondoContenitoreRisposte = document.querySelectorAll(".risposte")[1]
+    
+    // quando la domanda ha due risposte, nascondi il secondo contenitore
+    if (tutteRisposte.length === 2) {
+        // elimina il secondo (si assume che sia anche l'ultimo) contenitore
+        // di risposte. il contenitore di risposte si identifica con .risposte
+        secondoContenitoreRisposte.style.display = "none"
+    } 
+    // quando la domanda ha quattro risposte, mostra il secondo contenitore
+    else if (tutteRisposte.length === 4) {
+        secondoContenitoreRisposte.style.display = "block"
+    }
+
+    tutteRisposte.forEach(testoRisposta => {
+        bottoniRisposteEl[indiceRisposta].textContent = testoRisposta
+        indiceRisposta += 1
+    })
+
+}
+
+
+// TODO: inserire funzionalità per randomizzare l'inserimento della risposta corretta
+function ottieniTutteRisposte (domandaObj) {
+    const correctAnswer = domandaObj.correct_answer
+    const incorrectAnswers = domandaObj.incorrect_answers
+    const allAnswers = [correctAnswer, ...incorrectAnswers]
+    return allAnswers
+}
+
+function haiTerminatoDomande() {
+  return indiceDomandaAttuale === questions.length;
 }
