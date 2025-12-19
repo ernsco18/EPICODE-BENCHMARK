b68d36321cbae28713fa1f1a25b87d03c472fd70
Ernesto <ernesto.scorzelli77@gmail.com>
2025-12-19 13:22:12 +0100

    Finale

--- assets/js/review.js
+++ assets/js/review.js
@@ -1,27 +1,3 @@
-/* const stelle = document.querySelectorAll(".stella");
-let selectedRating = 0;
-
-function updateStars(value) {
-  stelle.forEach((star) => {
-    star.classList.toggle("active", star.dataset.value <= value);
-  });
-}
-
-stelle.forEach((star) => {
-  star.addEventListener("mouseenter", () => {
-    updateStars(star.dataset.value);
-  });
-
-  star.addEventListener("click", () => {
-    selectedRating = star.dataset.value;
-    updateStars(selectedRating);
-  });
-});
-
-document.querySelector(".valutazione").addEventListener("mouseleave", () => {
-  updateStars(selectedRating);
-});
- */
 const stelle = document.querySelectorAll(".stella");
 const stellaAccesa = "./assets/img/star.svg";
 const stellaSpenta = "./assets/img/star-blue.svg";
